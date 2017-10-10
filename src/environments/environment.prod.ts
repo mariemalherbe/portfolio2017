1e49dac34d4ed7262e86b0bf5a1f6af3caaafc5e
mariemalherbe <malherbemarie0@gmail.com>
2017-10-10 16:06:14 +0200

    Add environment.prod.ts for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAx7hjs-xEL2Gq-Dsbl7zeFT0TGtjP3aco',
+    authDomain: 'portfolio-6355b.firebaseapp.com',
+    databaseURL: 'https://portfolio-6355b.firebaseio.com',
+    projectId: 'portfolio-6355b',
+    storageBucket: 'portfolio-6355b.appspot.com',
+    messagingSenderId: '252852411069'
+  }
 };

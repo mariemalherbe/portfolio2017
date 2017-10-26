a32c4f3e4d1e0d7ae72c6f6afc67262dd6496f28
mariemalherbe <malherbemarie0@gmail.com>
2017-10-26 12:31:29 +0200

    update animations & portfoliPage details

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -9,25 +9,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss'],
-
-
-  animations: [
-        trigger("animationProject", [
-            state("inactive", style({
-                opacity: 0,
-                visibility: "hidden",
-                transform: 'translateY(100%)'
-            })),
-            state("active", style({
-                opacity: 1,
-                visibility: "visible",
-                transform: 'translateY(0%)'
-            })),
-            transition("inactive => active", animate("1500ms ease-in")),
-            transition("active => inactive", animate("2000ms ease-out")),
-        ])
-    ]
+  styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
 projects: FirebaseListObservable<any[]>;
@@ -51,22 +33,5 @@ projects: FirebaseListObservable<any[]>;
  onMouseOut(): void {
     this.colorTags = "white";
   }
-
-  scrollimateOptions: any = {
-    myScrollimation: {
-        currentState: "inactive", //currentState is required
-        states: [
-            {
-                method: "pxLeft", //this will be true when a total of 85% of the page has been scrolled
-                value: 1500,
-                state: "active",
-            },
-            {
-                method: "default",
-                state: "inactive"
-            }
-        ]
-    },
-}
 
 }
